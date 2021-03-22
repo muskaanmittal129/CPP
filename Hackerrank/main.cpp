a849998764b2b4f473303836bba092823ea35ccf
Muskaan Mittal <muskaanmittal129>
2021-03-22 16:05:09 +0530

    PDF viewer

--- Hackerrank/main.cpp
+++ Hackerrank/main.cpp
@@ -3,6 +3,22 @@ typedef long long ll;
 using namespace std;
 
 int main() {
-
+    int a[30],max=0;
+    for(int i=0; i<26; i++){
+        cin>>a[i];
+    }
+    string str;
+    cin>>str;
+    int b[30] ={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    for(int i=0; i<str.length();i++){
+        for(int j=0; j<26; j++){
+            if(str[i]==b[j]){
+                if(max<a[j]){
+                    max=a[j];
+                }
+            }
+        }
+    }
+    cout<<max*(str.length());
     return 0;
 }
