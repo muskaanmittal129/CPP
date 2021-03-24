ee6c2d289574bd59caa39a8f0e8ebb05112a58c3
Muskaan Mittal <muskaanmittal129>
2021-03-24 14:41:18 +0530

    #include <bits/stdc++.h>

---
+++ Hackerrank/Sequence-Equation .cpp
@@ -0,0 +1,22 @@
+//#include <bits/stdc++.h>
+//typedef long long ll;
+//using namespace std;
+//
+//int main() {
+//    int n, ans;
+//    cin>>n;
+//    vector<int> a(n), b(n);
+//    for(int i=1; i<=n; i++){
+//        cin>>a[i];
+//    }
+//    for(int i=1; i<=n; i++){
+//        ans = a[a[a[i]]];
+//
+//        b[ans] = a[i];
+//    }
+//    for(int i=1; i<=n; i++){
+//        cout<<b[i]<<"\n";
+//    }
+//
+//    return 0;
+//}
--- Hackerrank/main.cpp
+++ Hackerrank/main.cpp
@@ -3,45 +3,19 @@ typedef long long ll;
 using namespace std;
 
 int main() {
-    int n,k,q,value,q_value;
-    cin>>n>>k>>q;
-    int a[n];
-    for(int i=0; i<n; i++){
+    int n, ans;
+    cin>>n;
+    vector<int> a(n), b(n);
+    for(int i=1; i<=n; i++){
         cin>>a[i];
     }
-    if(k>n){
-        for(int i=1; i<=k-n; i++){
-            value = a[n-1];
-            int j = n-1;
-            while(j>0){
-                a[j] = a[j-1];
-                j--;
-            }
-            a[0] = value;
-        }
-    }
-    else{
-        int b[k];
-        for(int i=0; i<k; i++){
-            b[i] = a[n-1-i];
-        }
-        int j = n-1;
-        while(j-k >= 0){
-            a[j] = a[j - k];
-            j--;
-        }
-        while(j>=0){
-            for(int i=0; i<k; i++){
-                a[j] = b[i];
-                j--;
-            }
+    for(int i=1; i<=n; i++){
+        ans = a[a[a[i]]];
 
-        }
+        b[ans] = a[i];
     }
-
-    while(q--){
-        cin>>q_value;
-        cout<<a[q_value]<<"\n";
+    for(int i=1; i<=n; i++){
+        cout<<b[i]<<"\n";
     }
 
     return 0;
