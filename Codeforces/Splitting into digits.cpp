ee815a80f90e7aea1e07c188a85240de1e9f14aa
Muskaan Mittal <muskaanmittal129@gmail.com>
2021-05-03 21:22:07 +0530

    Splitting into digits

---
+++ Codeforces/Splitting into digits.cpp
@@ -0,0 +1,25 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//
+//int main() {
+//    FASTIO
+//    int n;
+//    cin >> n;
+//    cout << n << "\n";
+//    while(n--){
+//        cout << 1 << " ";
+//    }
+//
+//
+//
+//
+//
+//    return 0;
+//}
--- Codeforces/main.cpp
+++ Codeforces/main.cpp
@@ -10,32 +10,14 @@ using namespace std;
 
 int main() {
     FASTIO
-    int t;
-    cin >> t;
-    while(t--){
-      int n, x, flag = 0;
-      cin >> n >> x;
-      if(x == 1) flag = 1;
-      else{
-          int a[n], b[n];
-          for(int i = 0; i < n; i++){
-              cin >> a[i];
-          }
-          for(int i = 0; i < n; i++){
-              cin >> b[i];
-          }
-          sort(a, a + n);
-          sort(b, b + n, greater<int>());
-          for(int i = 0; i < n; i++){
-              if(a[i] + b[i] > x)
-              {flag = 1;
-                  break;}
-          }
-      }
-
-      if(flag == 1) cout << "NO" << "\n";
-      else cout << "YES" << "\n";
+    int n;
+    cin >> n;
+    cout << n << "\n";
+    while(n--){
+        cout << 1 << " ";
     }
+
+
 
 
 
