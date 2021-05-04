f011c531233652ee02ddbe0b16e23a2030f825e0
Muskaan Mittal <muskaanmittal129@gmail.com>
2021-05-04 22:07:51 +0530

    Kuroni and the Gifts

---
+++ Codeforces/Kuroni and the Gifts.cpp
@@ -0,0 +1,40 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--){
+//        int n;
+//        cin >> n;
+//        vector<int> a(n), b(n);
+//        for(int i = 0; i < n; i++){
+//            cin >> a[i];
+//        }
+//        for(int i = 0; i < n; i++){
+//            cin >> b[i];
+//        }
+//        sort(a.begin(), a.end());
+//        sort(b.begin(), b.end());
+//        for(int i = 0; i < n; i++){
+//            cout << a[i] << " ";
+//        }cout << "\n";
+//        for(int i = 0; i < n; i++){
+//            cout << b[i] << " ";
+//        }cout << "\n";
+//    }
+//
+//
+//
+//
+//
+//    return 0;
+//}
