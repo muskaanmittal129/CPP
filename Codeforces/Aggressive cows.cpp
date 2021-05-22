e5ecf04bb41c9bbfcd55c3c061efd39dfe765685
Muskaan Mittal <muskaanmittal129@gmail.com>
2021-05-23 01:10:57 +0530

    Aggressive cows

---
+++ Codeforces/Aggressive cows.cpp
@@ -0,0 +1,44 @@
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//#define ll              long long int
+//#define mod             1000000007
+//#define pi              3.141592653589793238
+//#define lcm(a,b)        (a/(__gcd(a,b)))*b
+//#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
+//
+//
+//int main() {
+//    FASTIO
+//    int t;
+//    cin >> t;
+//    while(t--){
+//        ll n, c;
+//        cin >> n >> c;
+//        ll arr[n];
+//        for(ll &i : arr){
+//            cin >> i;
+//        }
+//        sort(arr, arr+n);
+//        ll low = 0, high = arr[n-1], mid, left, cnt, ans;
+//        while(low <= high){
+//            mid = (low + high + 1) / 2;
+//            cnt = 1;
+//            left = 0;
+//            for(ll i = 1; i < n && cnt < c; i++){
+//                if(arr[i] - arr[left] >= mid){
+//                    cnt++;
+//                    left = i;
+//                }
+//            }
+//            if(cnt >= c){
+//                ans = mid;
+//                low = mid + 1;
+//            }
+//            else high = mid - 1;
+//        }
+//        cout << ans << "\n";
+//
+//    }
+//    return 0;
+//}
--- Codeforces/main.cpp
+++ Codeforces/main.cpp
@@ -7,26 +7,37 @@ using namespace std;
 #define lcm(a,b)        (a/(__gcd(a,b)))*b
 #define FASTIO          ios_base::sync_with_stdio(false);cin.tie(NULL);cout<<fixed;cout.precision(10);
 
-ll smallestprime(ll n){
-    for(ll i = 3; i*i <= n; i+=2){
-        if(n % i == 0)return i;
-    }
-    return n;
-}
 
 int main() {
     FASTIO
    int t;
     cin >> t;
     while(t--){
-        ll n, k;
-        cin >> n >> k;
-        if(n % 2 == 0)
-            cout << (n + 2*k)<<"\n";
-        else{
-            n += (2 * (k -1)) + smallestprime(n);
-            cout << n << "\n";
+        ll n, c;
+        cin >> n >> c;
+        ll arr[n];
+        for(ll &i : arr){
+            cin >> i;
         }
+        sort(arr, arr+n);
+        ll low = 0, high = arr[n-1], mid, left, cnt, ans;
+        while(low <= high){
+            mid = (low + high + 1) / 2;
+            cnt = 1;
+            left = 0;
+            for(ll i = 1; i < n && cnt < c; i++){
+                if(arr[i] - arr[left] >= mid){
+                    cnt++;
+                    left = i;
+                }
+            }
+            if(cnt >= c){
+                ans = mid;
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        cout << ans << "\n";
 
     }
     return 0;
